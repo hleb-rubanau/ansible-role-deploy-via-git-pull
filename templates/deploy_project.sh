8efdfc99c6b7316631bbfbf0ba619acb1da5dc32
Hleb Rubanau <g.rubanau@gmail.com>
2020-05-21 23:17:03 +0300

    fix another typo

--- templates/deploy_project.sh
+++ templates/deploy_project.sh
@@ -5,7 +5,7 @@ set -o pipefail
 
 {% if deployment_use_lockfile %}
 export DEPLOYMENT_LOCK_FILE="/var/run/deployments/{{ deployment_project }}.pid"
-{% end %}
+{% endif %}
 export DEPLOYMENT_COMMAND="{{ deployment_command }}"
 {% if deployment_git_upstream is defined %}
 export DEPLOYMENT_GIT_UPSTREAM="{{ deployment_git_upstream }}"
